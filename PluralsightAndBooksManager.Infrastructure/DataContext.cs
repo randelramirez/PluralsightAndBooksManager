29f15ca10d5aa8830bb3b6974bec35b99f7339d8
randelramirez <randelramirez1@gmail.com>
2016-07-27 00:32:55 +0800

    Add project files.

---
+++ PluralsightAndBooksManager.Infrastructure/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using PluralsightAndBooksManager.Core;
+using System.Data.Entity;
+
+namespace PluralsightAndBooksManager.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<CourseMaterial> Courses { get; set; }
+    }
+}
---
+++ PluralsightAndBooksManager.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PluralsightAndBooksManager.WebUI.Startup))]
+namespace PluralsightAndBooksManager.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
