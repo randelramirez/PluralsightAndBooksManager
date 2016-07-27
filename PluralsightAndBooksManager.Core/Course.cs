a450e1d3599fe30fb18dc7385bb7a6086c781f67
randelramirez <randelramirez1@gmail.com>
2016-07-28 00:28:29 +0800

    Removed IsInCollection in index view, Added Display attribute for Course, IsInCollection property

--- PluralsightAndBooksManager.Core/Course.cs
+++ PluralsightAndBooksManager.Core/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace PluralsightAndBooksManager.Core
 
         public string Title { get; set; }
 
+        [Display(Name = "Is in collection?")]
         public bool IsInCollection { get; set; }
     }   
 }
